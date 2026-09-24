c0f7b725bcc4905e62e97611ec3b526c8d4472d8
Oleksander Sychov <113937544+res200183@users.noreply.github.com>
2026-09-24 21:11:34 +0300

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,16 @@
+from app import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert b"Production Pipeline" in response.data
+
+
+def test_health():
+    client = app.test_client()
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.data == b"OK"
